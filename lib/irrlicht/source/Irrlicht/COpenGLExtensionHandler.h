a58beb0138d90f23bf2e98e9ec39cfef59fbad39
Benau <Benau@users.noreply.github.com>
2020-11-23 13:28:29 +0800

    Replace glew with glad with vulkan support

--- lib/irrlicht/source/Irrlicht/COpenGLExtensionHandler.h
+++ lib/irrlicht/source/Irrlicht/COpenGLExtensionHandler.h
@@ -14,17 +14,13 @@
 #include "os.h"
 
 #if defined(_IRR_COMPILE_WITH_SDL_DEVICE_)
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#define GL_GLEXT_LEGACY 1
-		#define GLX_GLXEXT_LEGACY 1
-	#else
-		#define GL_GLEXT_PROTOTYPES 1
-		#define GLX_GLXEXT_PROTOTYPES 1
-	#endif
-	#define NO_SDL_GLEXT
 	#include <SDL_video.h>
-	#include <SDL_opengl.h>
-	#include "glext.h"
+	#include "glad/gl.h"
+#ifdef _IRR_WINDOWS_API_
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+#endif
+
 #elif defined(_IRR_WINDOWS_API_)
 	// include windows headers for HWND
 	#define WIN32_LEAN_AND_MEAN
